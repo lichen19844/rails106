419dd9922fbb2ec207ebb4082886a9e16cf2b77a
lichen <119449234@qq.com>
2017-09-06 14:21:34 +0800

    optimize navbar

--- app/assets/javascripts/navbar-fixed-top.js
+++ app/assets/javascripts/navbar-fixed-top.js
@@ -1,16 +1,4 @@
 
-
-
-    // $(window).scroll(function() { // check if scroll event happened
-    //     if ($(document).scrollTop() > 50) { 
-    //         $(".navbar-fixed-top").css("background-color", "#212121"); 
-    //     } else {
-    //         $(".navbar-fixed-top").css("background-color", "transparent"); 
-    //     }
-    // });
-
-
-
       $(document).ready(function(){
          $(".navbar-fixed-top").css("background", "transparent"); 
          $(".navbar-fixed-top").css("border-color", "transparent"); 
