35b33dac30da023bd83040ccc36d29da377c0469
lichen <119449234@qq.com>
2017-09-06 23:26:20 +0800

    identifies  one vs more

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, length: { maximum: 25 }
+
+  has_many :identifies
+
+
 end
