7ee098645027c2fa9ac2b02ca12cfe7e8b4c197f
lichen <119449234@qq.com>
2017-09-09 23:47:38 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+	before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
 	def index
 		@groups = Group.all
 	end
@@ -23,19 +25,10 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:id])
 
-		if current_user != @group.user
-			redirect_to root_path, alert: "You have no promission."
-		end
 	end
 
 	def update
-		@group = Group.find(params[:id])
-
-		if current_user != @group.user
-			redirect_to root_path, alert: "You have no promission."
-		end
 
 		if @group.update(group_params)
 			redirect_to groups_path, notice: "Update Success."		
@@ -45,11 +38,6 @@ class GroupsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:id])
-
-		if current_user != @group.user
-			redirect_to root_path, alert: "You have no promission."
-		end
 
 		@group.destroy
 		redirect_to groups_path
@@ -60,5 +48,14 @@ class GroupsController < ApplicationController
 
 	def group_params
 		params.require(:group).permit(:title, :description)
+	end
+
+	def find_group_and_check_permission
+
+		@group = Group.find(params[:id])
+
+		if current_user != @group.user
+			redirect_to root_path, alert: "You have no promission."
+		end		
 	end
 end
